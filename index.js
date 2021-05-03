907a4465fd6ddd9b52a66209ca5b2854f99a94d9
ZAELROOT <josu1918@gmail.com>
2021-05-04 00:42:12 +0900

    define function of Types

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "SuHyeon", age = 24, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi("suhyeon", 29, "male");
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,10 @@
+const name = "SuHyeon",
+    age = 24,
+    gender = "male";
+
+const sayHi = (name: string,age: number,gender:string) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+sayHi("suhyeon",29,"male");
+export { };  
