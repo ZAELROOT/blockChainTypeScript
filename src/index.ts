01d1b0330c2d19b361685d6e6966e73bf41d23d1
ZAELROOT <josu1918@gmail.com>
2021-05-04 18:04:41 +0900

    practice about interface

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-const name = "SuHyeon",
-    age = 24,
-    gender = "male";
-
-const sayHi = (name: string, age: number, gender: string): void => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
 }
 
-sayHi("suhyeon", 27, "male");
+const person = {
+    name: "suhyeon",
+    age: 27,
+    gender:"male"
+}
+
+const sayHi = (person:Human): void => {
+    console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
+}
+
+sayHi(person);
 export { };  
