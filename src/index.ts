a9cf3562970d78a540cac88a0629c3f7d95219cc
ZAELROOT <josu1918@gmail.com>
2021-05-04 17:47:30 +0900

    Create directories & set tsc-watch dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ const name = "SuHyeon",
     age = 24,
     gender = "male";
 
-const sayHi = (name: string,age: number,gender:string) => {
+const sayHi = (name: string, age: number, gender: string): void => {
     console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 }
 
-sayHi("suhyeon",29,"male");
+sayHi("suhyeon", 27, "male");
 export { };  
