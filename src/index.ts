9bfb4d0f75e68e724b91934de52361a1e49a9ef5
ZAELROOT <josu1918@gmail.com>
2021-05-04 18:12:52 +0900

    practice about class

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "suhyeon",
-    age: 27,
-    gender:"male"
-}
+const suHyeon = new Human("suHyeon", 27, "male");
 
-const sayHi = (person:Human): void => {
+const sayHi = (person: Human): void => {
     console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
 }
 
-sayHi(person);
+sayHi(suHyeon);
 export { };  
